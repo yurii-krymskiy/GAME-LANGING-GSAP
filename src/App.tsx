0e79b9904a91fb50e44a2705820cf8a346a4798c
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-16 00:24:52 +0200

    features

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 }
